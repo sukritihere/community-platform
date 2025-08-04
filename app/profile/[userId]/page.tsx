496a73e8d2695e6d298dc94e9bab716b8f0d5b32
sukritihere <sukritisingh9469@gmail.com>
2025-08-04 11:49:32 +0530

    fix: url fixed

--- app/profile/[userId]/page.tsx
+++ app/profile/[userId]/page.tsx
@@ -1,5 +1,5 @@
 export async function generateStaticParams() {
-  const res = await fetch("http://localhost:5000/api/users");
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/users`);
   const data = await res.json();
   return data.users.map((user: any) => ({ userId: user._id }));
 }
--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: "export",
   eslint: {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  env: {
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  },
 };
 
 module.exports = nextConfig;
